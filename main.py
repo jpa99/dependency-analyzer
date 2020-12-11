b9c70ea451f6a9fcd27db9f9748f79e75429e2f9
jpa99 <joelpabraham@gmail.com>
2020-12-11 15:46:49 -0600

    parsed initial test script

--- main.py
+++ main.py
@@ -13,14 +13,10 @@ PY_LANGUAGE = tree_sitter.Language('build/my-languages.so', 'python')
 parser = tree_sitter.Parser()
 parser.set_language(PY_LANGUAGE)
 
-tree = parser.parse(bytes("""
-import math
-from bar import *
-
-def foo():
-    if test == math.pow(2, 3):
-        baz()
-""", "utf8"))
+with open('test/foo.py', 'r') as file:
+	file_text = file.read()
+	
+tree = parser.parse(bytes(file_text, 'utf8'))
 
 string = tree.root_node.sexp()
 print(string)
---
+++ test/bar.py
@@ -0,0 +1,5 @@
+import bar
+import sys
+
+bar.hello()
+sys.exit(1)
